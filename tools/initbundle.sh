4f05431982cb497648d2690b7b4797be3c2f3192
anetczuk <anetczuk@o2.pl>
2021-10-30 23:46:28 +0200

    Configure local bundle allowing previewing of GitHub themes

---
+++ tools/initbundle.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -eu
+
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+
+ROOT_DIR=${SCRIPT_DIR}/..
+
+SITE_DIR=${ROOT_DIR}/docs
+
+BUILD_DIR=${ROOT_DIR}/tmp/local
+
+
+mkdir -p ${BUILD_DIR
+
+cd ${BUILD_DIR}
+
+
+cp $SITE_DIR/Gemfile .
+#### creates simple Gemfile
+## bundle init
+
+#### creates '.bundle/config' and 'vendor' dir
+bundle install --path vendor/bundle
+
+#### installs binaries to 'vendor' subdir
+bundle add jekyll
+
+
+#### following steps are not necessary
+
+#### create content from minimal theme from template: '_posts', '_config.yml', '404.html', 'about.markdown', 'index.markdown'
+## bundle exec jekyll new --force --skip-bundle .
+
+#### installs additional binaries to 'vendor' subdir
+## bundle install
+
+#### build static site and run server
+## bundle exec jekyll serve
--- tools/startlocal.sh
+++ tools/startlocal.sh
@@ -5,6 +5,7 @@ set -eu
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+
 ROOT_DIR=${SCRIPT_DIR}/..
 
 SITE_DIR=${ROOT_DIR}/docs
@@ -12,10 +13,13 @@ SITE_DIR=${ROOT_DIR}/docs
 BUILD_DIR=${ROOT_DIR}/tmp/local
 
 
-cd ${SITE_DIR}
+cd ${BUILD_DIR}
+
+bundle exec jekyll serve -s ${SITE_DIR}
 
 
-mkdir -p ${BUILD_DIR}
+## cd ${SITE_DIR}
 
+## mkdir -p ${BUILD_DIR}
 
-jekyll clean -d ${BUILD_DIR} && jekyll serve -d ${BUILD_DIR}
+## jekyll clean -d ${BUILD_DIR} && jekyll serve -d ${BUILD_DIR}
